682beeefb216c96e62fe219f27fe98b2b5047f4e
SOLab Lecture <lecture@solab.co.kr>
2012-09-12 01:56:01 +0900

    Initial Commit

---
+++ iOS ProjectTests/iOS_ProjectTests.h
@@ -0,0 +1,13 @@
+//
+//  iOS_ProjectTests.h
+//  iOS ProjectTests
+//
+//  Created by JINYOUNG PARK on 12. 9. 12..
+//  Copyright (c) 2012년 Zest of Development. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iOS_ProjectTests : SenTestCase
+
+@end
---
+++ iOS ProjectTests/iOS_ProjectTests.m
@@ -0,0 +1,32 @@
+//
+//  iOS_ProjectTests.m
+//  iOS ProjectTests
+//
+//  Created by JINYOUNG PARK on 12. 9. 12..
+//  Copyright (c) 2012년 Zest of Development. All rights reserved.
+//
+
+#import "iOS_ProjectTests.h"
+
+@implementation iOS_ProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iOS ProjectTests");
+}
+
+@end
